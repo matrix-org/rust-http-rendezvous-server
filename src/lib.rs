ada45288be05339ace1d3cae1f3f7746e588d8bb
Quentin Gliech <quenting@element.io>
2022-09-09 12:43:57 +0200

    Expire entries & reply with additional headers

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,13 @@
 #![warn(clippy::pedantic)]
 #![allow(clippy::trait_duplication_in_bounds)]
 
-use std::{borrow::Cow, collections::HashMap, ops::Deref, sync::Arc, time::SystemTime};
+use std::{
+    borrow::Cow,
+    collections::HashMap,
+    ops::Deref,
+    sync::Arc,
+    time::{Duration, SystemTime},
+};
 
 use axum::{
     body::HttpBody,
@@ -29,7 +35,7 @@ use axum::{
 };
 use base64ct::Encoding;
 use bytes::Bytes;
-use headers::{ContentType, ETag, HeaderName, IfNoneMatch, LastModified};
+use headers::{ContentType, ETag, Expires, HeaderName, IfNoneMatch, LastModified};
 use mime::Mime;
 use sha2::Digest;
 use tokio::sync::RwLock;
@@ -37,24 +43,34 @@ use uuid::Uuid;
 
 // TODO: config?
 const MAX_BYTES: u64 = 4096;
+const TTL: Duration = Duration::from_secs(60);
 
-// TODO: expire the sessions
 struct Session {
     hash: [u8; 32],
     data: Bytes,
     content_type: Mime,
     last_modified: SystemTime,
+    expires: SystemTime,
 }
 
 impl Session {
     fn new(data: Bytes, content_type: Mime) -> Self {
         let hash = sha2::Sha256::digest(&data).into();
+        let now = SystemTime::now();
         Self {
             hash,
             data,
             content_type,
-            last_modified: SystemTime::now(),
+            expires: now + TTL,
+            last_modified: now,
         }
+    }
+
+    fn update(&mut self, data: Bytes, content_type: Mime) {
+        self.hash = sha2::Sha256::digest(&data).into();
+        self.data = data;
+        self.content_type = content_type;
+        self.last_modified = SystemTime::now();
     }
 
     fn content_type(&self) -> ContentType {
@@ -70,12 +86,41 @@ impl Session {
     fn last_modified(&self) -> LastModified {
         self.last_modified.into()
     }
+
+    fn expires(&self) -> Expires {
+        self.expires.into()
+    }
+
+    fn typed_headers(
+        &self,
+    ) -> (
+        TypedHeader<ETag>,
+        TypedHeader<Expires>,
+        TypedHeader<LastModified>,
+    ) {
+        (
+            TypedHeader(self.etag()),
+            TypedHeader(self.expires()),
+            TypedHeader(self.last_modified()),
+        )
+    }
 }
 
 #[derive(Clone, Default)]
 struct Sessions {
     // TODO: is that global lock alright?
     inner: Arc<RwLock<HashMap<Uuid, Session>>>,
+}
+
+impl Sessions {
+    async fn insert(self, id: Uuid, session: Session, ttl: Duration) {
+        self.inner.write().await.insert(id, session);
+        // TODO: cancel this task when an item gets deleted
+        tokio::task::spawn(async move {
+            tokio::time::sleep(ttl).await;
+            self.inner.write().await.remove(&id);
+        });
+    }
 }
 
 impl Deref for Sessions {
@@ -121,9 +166,9 @@ struct AppState {
 }
 
 impl AppState {
-    fn new(prefix: impl Into<Cow<'static, str>>) -> Self {
+    fn new(prefix: impl Into<Cow<'static, str>>, sessions: Sessions) -> Self {
         Self {
-            sessions: Sessions::default(),
+            sessions,
             prefix: Prefix::new(prefix),
         }
     }
@@ -141,37 +186,46 @@ async fn new_session(
     let content_type =
         content_type.map_or(mime::APPLICATION_OCTET_STREAM, |TypedHeader(c)| c.into());
     let session = Session::new(payload, content_type);
-    sessions.write().await.insert(id, session);
+    let headers = session.typed_headers();
+    sessions.insert(id, session, TTL).await;
 
     // TODO: actually join with the prefix, to prevent accidental double slashes
     let location = prefix.location(&id);
-    let headers = [
+    let additional_headers = [
         (LOCATION, location),
         (
             HeaderName::from_static("x-max-bytes"),
             MAX_BYTES.to_string(),
         ),
     ];
-    (StatusCode::NO_CONTENT, headers)
+    (StatusCode::NO_CONTENT, headers, additional_headers)
 }
+
+async fn delete_session(State(sessions): State<Sessions>, Path(id): Path<Uuid>) -> StatusCode {
+    if sessions.write().await.remove(&id).is_some() {
+        StatusCode::NO_CONTENT
+    } else {
+        StatusCode::NOT_FOUND
+    }
+}
+
 async fn update_session(
     State(sessions): State<Sessions>,
     Path(id): Path<Uuid>,
     content_type: Option<TypedHeader<ContentType>>,
     ContentLengthLimit(payload): ContentLengthLimit<Bytes, MAX_BYTES>,
-) -> StatusCode {
-    if sessions.read().await.get(&id).is_none() {
-        return StatusCode::NOT_FOUND;
-    }
-
+) -> Response {
     // TODO: check the if-match header
     // TODO: probably missing some response headers
 
     let content_type =
         content_type.map_or(mime::APPLICATION_OCTET_STREAM, |TypedHeader(c)| c.into());
-    let session = Session::new(payload, content_type);
-    sessions.write().await.insert(id, session);
-    StatusCode::ACCEPTED
+    if let Some(session) = sessions.write().await.get_mut(&id) {
+        session.update(payload, content_type);
+        (StatusCode::ACCEPTED, session.typed_headers()).into_response()
+    } else {
+        StatusCode::NOT_FOUND.into_response()
+    }
 }
 
 async fn get_session(
@@ -186,18 +240,16 @@ async fn get_session(
         return StatusCode::NOT_FOUND.into_response();
     };
 
-    let etag = session.etag();
     if let Some(TypedHeader(if_none_match)) = if_none_match {
-        if if_none_match.precondition_passes(&etag) {
+        if if_none_match.precondition_passes(&session.etag()) {
             return StatusCode::NOT_MODIFIED.into_response();
         }
     }
 
     (
         StatusCode::OK,
+        session.typed_headers(),
         TypedHeader(session.content_type()),
-        TypedHeader(session.last_modified()),
-        TypedHeader(etag),
         session.data.clone(),
     )
         .into_response()
@@ -211,10 +263,15 @@ where
     <B as HttpBody>::Error: std::error::Error + Send + Sync,
 {
     let prefix = prefix.into();
-    let state = AppState::new(prefix.clone());
+    let sessions = Sessions::default();
+
+    let state = AppState::new(prefix.clone(), sessions);
     let router = Router::with_state(state)
         .route("/", post(new_session))
-        .route("/:id", get(get_session).put(update_session));
+        .route(
+            "/:id",
+            get(get_session).put(update_session).delete(delete_session),
+        );
 
     Router::new().nest(&prefix, router)
 }
@@ -229,6 +286,14 @@ mod tests {
     };
     use tower::util::ServiceExt;
 
+    async fn advance_time(duration: Duration) {
+        tokio::task::yield_now().await;
+        tokio::time::pause();
+        tokio::time::advance(duration).await;
+        tokio::time::resume();
+        tokio::task::yield_now().await;
+    }
+
     #[tokio::test]
     async fn test_post_and_get() {
         let app = router("/");
@@ -242,20 +307,53 @@ mod tests {
 
         let response = app.clone().oneshot(request).await.unwrap();
         assert_eq!(response.status(), StatusCode::NO_CONTENT);
-        let location = response.headers().get(LOCATION).unwrap();
+        let location = response.headers().get(LOCATION).unwrap().to_str().unwrap();
 
-        let request = Request::get(location.to_str().unwrap())
-            .body(String::new())
-            .unwrap();
+        let request = Request::get(location).body(String::new()).unwrap();
 
-        let response = app.oneshot(request).await.unwrap();
+        let response = app.clone().oneshot(request).await.unwrap();
         assert_eq!(response.status(), StatusCode::OK);
         assert_eq!(
             response.headers().get(CONTENT_TYPE).unwrap(),
             "application/json"
         );
 
+        // Let the entry expire
+        advance_time(TTL + Duration::from_secs(1)).await;
+
         let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
         assert_eq!(&body[..], br#"{"hello": "world"}"#);
+
+        let request = Request::get(location).body(String::new()).unwrap();
+        let response = app.oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[tokio::test]
+    async fn test_post_delete_and_get() {
+        let app = router("/");
+
+        let body = r#"{"hello": "world"}"#.to_string();
+        let request = Request::post("/")
+            .header(CONTENT_TYPE, "application/json")
+            .header(CONTENT_LENGTH, body.len())
+            .body(body)
+            .unwrap();
+
+        let response = app.clone().oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NO_CONTENT);
+        let location = response.headers().get(LOCATION).unwrap().to_str().unwrap();
+
+        let request = Request::get(location).body(String::new()).unwrap();
+        let response = app.clone().oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let request = Request::delete(location).body(String::new()).unwrap();
+        let response = app.clone().oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NO_CONTENT);
+
+        let request = Request::get(location).body(String::new()).unwrap();
+        let response = app.oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
     }
 }
