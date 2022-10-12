e385026e59958b3d63d37d41923375951cbf9d68
Quentin Gliech <quenting@element.io>
2022-10-12 16:43:00 +0200

    Add command line arguments to the standalone server

--- server/src/main.rs
+++ server/src/main.rs
@@ -16,18 +16,39 @@
 #![deny(clippy::all)]
 #![warn(clippy::pedantic)]
 
-use std::net::SocketAddr;
+use clap::Parser;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+#[derive(Parser)]
+struct Options {
+    /// Address on which to listen
+    #[arg(short, long, default_value_t = IpAddr::V4(Ipv4Addr::LOCALHOST))]
+    address: IpAddr,
+
+    /// Port on which to listen
+    #[arg(short, long, default_value_t = 8090)]
+    port: u16,
+
+    /// Path prefix on which to mount the rendez-vous server
+    #[arg(long)]
+    prefix: Option<String>,
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = matrix_http_rendezvous::router("");
+    let options = Options::parse();
+    let prefix = options.prefix.unwrap_or_default();
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8090));
+    let addr = SocketAddr::from((options.address, options.port));
+
+    let service = matrix_http_rendezvous::router(&prefix);
+
+    tracing::info!("Listening on http://{addr}");
 
     hyper::Server::bind(&addr)
-        .serve(app.into_make_service())
+        .serve(service.into_make_service())
         .await
         .unwrap();
 }
